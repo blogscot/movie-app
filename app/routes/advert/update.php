46646276c3742f3c7ae119563d1a422de14d213f
iain <iain.diamond@gmail.com>
2016-03-01 18:00:43 +0000

    Update movie details

--- app/routes/advert/update.php
+++ app/routes/advert/update.php
@@ -7,3 +7,46 @@ $app->get('/update/:id', function($id) use ($app) {
     'advert' => $advert
   ]);
 })->name('advert.update');
+
+$app->post('/update/:id', function($id) use ($app) {
+
+    $request = $app->request;
+
+    $title = $request->post('title');
+    $price = $request->post('price');
+    $category = $request->post('category');
+    $description = $request->post('description');
+
+    $v = $app->validation;
+
+    $v->validate([
+      'title' => [$title, 'required|min(3)|max(40)'],
+      'price' => [$price, 'required|number|min(0)'],
+      'category' => [$category, 'required|max(20)'],
+      'description' => [$description, 'max(512)']
+    ]);
+
+    if ($v->passes()) {
+
+      // recalculate daily ad rate
+      $ad_rate = str_word_count($description) / 12 + 10;
+
+      $advert = $app->advert->where('id', $id)->first();
+      $advert->title = $title;
+      $advert->price = $price;
+      $advert->category = $category;
+      $advert->description = $description;
+      $advert->ad_rate = $ad_rate;
+      $advert->user_id = $app->auth->id;
+      $advert->save();
+
+      $app->flash('global', 'Advert updated!');
+      $app->response->redirect($app->urlFor('advert.viewall'));
+    }
+
+    $app->render('advert/add.php', [
+       'errors' => $v->errors(),
+       'request' => $request
+    ]);
+
+})->name('advert.update.post');
--- app/views/advert/update.php
+++ app/views/advert/update.php
@@ -3,5 +3,55 @@
 {% block title %}Update Advert{% endblock %}
 
 {% block content %}
-  Update Advert
+<form class="login_form" action="{{ urlFor('advert.update.post', {id: advert.id}) }}" method="post" autocomplete="off">
+   <ul class="center-block">
+     <h2>Update Advert</h2>
+      <li>
+         <label for="title">Title: </label>
+         <input type="text" name="title" id="title"
+         value='{{ advert.title }}'>
+         {% if errors.has('title') %} {{ errors.first('title') }} {% endif %}
+      </li>
+      </li>
+      <li>
+         <label for="price">Price: </label>
+         <input type="number" step="0.01" name="price" id="price"
+         value="{{ advert.price|number_format(2,'.',',') }}">
+         {% if errors.has('price') %} {{ errors.first('price') }} {% endif %}
+      </li>
+      <li>
+        <label for="title">Category: </label>
+        <select class="movie_category" name="category">
+          <option value="Romance"
+          {% if advert.category == 'Romance' %}
+            selected
+          {% endif %}>Romance</option>
+          <option value="Thriller"
+            {% if advert.category == 'Thriller' %}
+              selected
+            {% endif %}>Thriller</option>
+          <option value="Comedy"
+            {% if advert.category == 'Comedy' %}
+              selected
+            {% endif %}>Comedy</option>
+          <option value="Horror"
+            {% if advert.category == 'Horror' %}
+              selected
+            {% endif %}>Horror</option>
+          <option value="Scifi"
+            {% if advert.category == 'Scifi' %}
+              selected
+            {% endif %}>SciFi</option>
+        </select>
+      </li>
+      <li>
+        <label for="title">Description: </label>
+        <textarea name="description" rows="8" cols="40">{{ advert.description }}</textarea>
+      </li>
+      <li>
+        <button class="btn btn-primary" type="submit">Update</button>
+      </li>
+   </ul>
+   <input type="hidden" name="{{ csrf_key }}" value="{{ csrf_token }}">
+</form>
 {% endblock %}
