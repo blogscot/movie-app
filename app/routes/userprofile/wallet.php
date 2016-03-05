a4be216ccba02dd4ca0e9863d8bc7b57473c4136
iain <iain.diamond@gmail.com>
2016-03-05 12:55:51 +0000

    User can add funds to wallet

--- app/routes/userprofile/wallet.php
+++ app/routes/userprofile/wallet.php
@@ -7,3 +7,28 @@ $app->get('/userwallet', $authenticated(), function() use ($app) {
     'wallet' => $wallet
   ]);
 })->name('user.wallet');
+
+$app->post('/userwallet', $authenticated(), function() use ($app) {
+  $request = $app->request;
+  $v = $app->validation;
+
+  $funds = $request->post('funds');
+
+  $v->validate([
+    'funds' => [$funds, 'required|number|min(0)'],
+  ]);
+
+  if ($v->passes()) {
+    $wallet = $app->wallet->get_user_balance($app->auth->id);
+    $wallet->balance += $funds;
+    $wallet->save();
+
+    $app->flash('global', 'New funds added!');
+    $app->response->redirect($app->urlFor('user.wallet'));
+  }
+
+  $app->render('advert/add.twig', [
+     'errors' => $v->errors(),
+     'request' => $request
+  ]);
+})->name('user.wallet.post');
