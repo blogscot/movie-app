2af3d777fcada1c5dcafdcdbb4315e19a3a3d53a
iain <iain.diamond@gmail.com>
2016-03-21 23:35:02 +0000

    Add Bootstrap form styles to Update Advert page

--- app/routes/advert/update.php
+++ app/routes/advert/update.php
@@ -14,7 +14,7 @@ $app->post('/update/:id', function($id) use ($app) {
 
     $title = $request->post('title');
     $price = $request->post('price');
-    $image_url = $request->post('image_url');
+    $fileToUpload = $_FILES["fileToUpload"]["name"];
     $category = $request->post('category');
     $description = $request->post('description');
 
@@ -23,28 +23,33 @@ $app->post('/update/:id', function($id) use ($app) {
     $v->validate([
       'title' => [$title, 'required|min(3)|max(40)'],
       'price' => [$price, 'required|number|min(0)'],
-      'image_url' => [$image_url, 'required|url|max(256)'],
+      'fileToUpload' => [$fileToUpload, 'required|max(128)'],
       'category' => [$category, 'required|max(20)'],
       'description' => [$description, 'max(512)']
     ]);
 
     if ($v->passes()) {
 
-      // recalculate daily ad rate
-      $ad_rate = $app->advert->calculate_ad_rate($description);
+      if (uploadImageFile($app) != false) {
+        // recalculate daily ad rate
+        $ad_rate = $app->advert->calculate_ad_rate($description);
 
-      $advert = $app->advert->where('id', $id)->first();
-      $advert->title = $title;
-      $advert->price = $price;
-      $advert->image_url = $image_url;
-      $advert->category = $category;
-      $advert->description = $description;
-      $advert->ad_rate = $ad_rate;
-      $advert->seller_id = $app->auth->id;
-      $advert->save();
+        $advert = $app->advert->where('id', $id)->first();
+        $advert->title = $title;
+        $advert->price = $price;
+        $advert->image_url = $app->config->get('app.uploads') . $fileToUpload;
+        $advert->category = $category;
+        $advert->description = $description;
+        $advert->ad_rate = $ad_rate;
+        $advert->seller_id = $app->auth->id;
+        $advert->save();
 
-      $app->flash('global', 'Advert updated!');
-      $app->response->redirect($app->urlFor('advert.viewbyuser'));
+        $app->flash('global', 'Advert updated!');
+        $app->response->redirect($app->urlFor('advert.viewbyuser'));
+      } else {
+        // display upload flash error message
+        return $app->response->redirect($app->urlFor('advert.add'));
+      }
     }
 
     $app->render('advert/add.twig', [
--- public/js/preview.js
+++
@@ -1,9 +0,0 @@
-// When a user creates a new advert, allow them to preview the image url
-$(document).ready(function() {
-  $('#preview-btn').on('click', function() {
-    var url = $('#image_url').val();
-    if (url != "") {
-      $('img').attr('src', url);
-    }
-  });
-});
