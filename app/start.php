5b6bbeb81a8189ebb4cf144129954124d6d6ae4a
iain <iain.diamond@gmail.com>
2016-04-10 18:22:58 +0100

    Add tooltips

--- app/start.php
+++ app/start.php
@@ -25,7 +25,7 @@ session_start();
 date_default_timezone_set("Europe/London");
 
 error_reporting(E_ALL);
-ini_set('display_errors', 'Off');
+ini_set('display_errors', 'On');
 
 define('INC_ROOT', dirname(__DIR__));
 
